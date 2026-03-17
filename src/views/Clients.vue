6c99d905044d84c91ad4e1a02612909252260517
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2026-03-17 20:19:23 +1300

    Refactor client management logic and improve code readability in Clients.vue

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -1,39 +1,58 @@
 <script setup>
-import { supabase } from '@/services/supabase';
-import { onMounted, ref } from 'vue';
+import { supabase } from '@/services/supabase'
+import { onMounted, ref } from 'vue'
 
-const name = ref("");
-const email = ref("");
-const address = ref("");
+const clients = ref([])
 
-async function addClient() {
+const name = ref('')
+const email = ref('')
+const address = ref('')
 
-  const { data: userData } = await supabase.auth.getUser()
-
-  console.log("USER:", userData.user.id)
-
-  const { data, error } = await supabase
-    .from("clients")
-    .insert([
-      {
-        user_id: userData.user.id,
-        name: name.value,
-        email: email.value,
-        address: address.value
-      }
-    ])
-
-  console.log("RESULT:", data, error)
+async function getClients() {
+  const { data, error } = await supabase.from('clients').select('*')
+  console.log('CLIENTS:', data, error)
+  clients.value = data
 }
 
+async function addClient() {
+  if (!name.value) {
+    alert('Client name required')
+    return
+  }
+  const { data: userData } = await supabase.auth.getUser()
+  console.log('USER:', userData.user.id)
+  const { data, error } = await supabase.from('clients').insert([
+    {
+      user_id: userData.user.id,
+      name: name.value,
+      email: email.value,
+      address: address.value,
+    },
+  ])
+
+  console.log('RESULT:', data, error)
+
+  await getClients()
+  name.value = ''
+  email.value = ''
+  address.value = ''
+}
+
+onMounted(() => {
+  getClients()
+})
 </script>
 
 <template>
-<h1>Clients</h1>
-<form @submit.prevent="addClient">
-    <input type="text" v-model="name" placeholder="Client Name">
-    <input type="text" v-model="email" placeholder="Email">
-    <input type="text" v-model="address" placeholder="Address">
+  <h1>Clients</h1>
+  <form @submit.prevent="addClient">
+    <input type="text" v-model="name" placeholder="Client Name" />
+    <input type="text" v-model="email" placeholder="Email" />
+    <input type="text" v-model="address" placeholder="Address" />
     <button>Add Client</button>
-</form>
+  </form>
+
+  <ul>
+    <li v-for="client in clients" :key="client.id">{{ client.name }} {{ client.email }}</li>
+  </ul>
 </template>
